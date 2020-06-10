4d375dce6feda4e55356d713fc536f6c3e12bbfd
GopherTy <54793772+GopherTy@users.noreply.github.com>
2020-06-10 17:24:00 +0800

    token example
    token auth

---
+++ golang/code/token/token.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/twinj/uuid"
+)
+
+// TokenDetails .
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUUID   string
+	RefreshUUID  string
+	AtExpires    int64
+	RtExpires    int64
+}
+
+// CreateToken .
+func CreateToken(userid int64) (td *TokenDetails, err error) {
+	td = &TokenDetails{}
+
+	// 访问 token 过期时间和 uuid
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUUID = uuid.NewV4().String()
+
+	// 刷新 token
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RefreshUUID = uuid.NewV4().String()
+
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUUID
+	atClaims["user_id"] = userid
+	atClaims["exp"] = td.AtExpires
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+		return
+	}
+
+	//Creating Refresh Token
+	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf") //this should be in an env file
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUUID
+	rtClaims["user_id"] = userid
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// CreateAuth .
+func CreateAuth(userid int64, td *TokenDetails) (err error) {
+	at := time.Unix(td.AtExpires, 0)
+	rt := time.Unix(td.RtExpires, 0)
+	now := time.Now()
+
+	errAccess := client.Set(td.AccessUUID, strconv.Itoa(int(userid)), at.Sub(now)).Err()
+	if errAccess != nil {
+		return
+	}
+
+	errRefresh := client.Set(td.RefreshUUID, strconv.Itoa(int(userid)), rt.Sub(now)).Err()
+	if errRefresh != nil {
+		return
+	}
+
+	return
+}
+
+// Todo .
+type Todo struct {
+	UserID uint64 `json:"user_id"`
+	Title  string `json:"title"`
+}
+
+// ExtractToken .
+func ExtractToken(r *http.Request) (str string) {
+	bearToken := r.Header.Get("Authorization")
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+// VerifyToken .
+func VerifyToken(r *http.Request) (token *jwt.Token, err error) {
+	tokenStr := ExtractToken(r)
+	token, err = jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("%v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+
+	if err != nil {
+		return
+	}
+
+	return
+}
+
+// TokenValid .
+func TokenValid(r *http.Request) (err error) {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return errors.New("token invalid")
+	}
+	return
+}
+
+// AccessDetails .
+type AccessDetails struct {
+	AccessUUID string
+	UserID     uint64
+}
+
+// ExtractTokenMetadata .
+func ExtractTokenMetadata(r *http.Request) (access *AccessDetails, err error) {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUUID, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, err
+		}
+		userID, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		access.AccessUUID = accessUUID
+		access.UserID = userID
+		return access, err
+	}
+
+	return
+}
+
+// FetchAuth .
+func FetchAuth(auth *AccessDetails) (userID uint64, err error) {
+	userid, err := client.Get(auth.AccessUUID).Result()
+	if err != nil {
+		return
+	}
+
+	userID, err = strconv.ParseUint(userid, 10, 64)
+	if err != nil {
+		return
+	}
+	return
+}
+// https://www.nexmo.com/blog/2020/03/13/using-jwt-for-authentication-in-a-golang-application-dr
